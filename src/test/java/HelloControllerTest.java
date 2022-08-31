e2f781368b77044011455585bfd09e014ad614e6
knoxa <knoxaleet1@gmail.com>
2022-09-01 01:40:15 +0300

    add wrong test for check Jenkins

--- src/test/java/HelloControllerTest.java
+++ src/test/java/HelloControllerTest.java
@@ -1,9 +1,15 @@
+import home.task.configuration.SpringConfig;
+import home.task.controller.HelloController;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.Extension;
+
 
 public class HelloControllerTest {
+
     @Test
     public void checkTest() {
-        Assertions.assertEquals(true, true);
+        Assertions.assertEquals(true, false);
     }
 }
